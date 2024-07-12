554a7438bfdff356eb7285fb22898ccb591330c5
Mohammad Anas <Mohammad Anas@DESKTOP-VIT8S0V>
2024-07-12 17:14:34 +0500

    complete sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,16 +6,16 @@ function Header() {
     <div className='bg-slate-200 shadow-md'>
     <header className='flex items-center justify-between p-3 max-w-6xl mx-auto'>
       <Link to="/" >
-        <h1 className='font-["Outfit"] flex flex-wrap items-baseline sm:items-end '>
-            <span className='text-xl sm:text-4xl text-transparent bg-clip-text font-[400] bg-gradient-to-r from-[#22c1c3] from-40% to-[#970404]'>Settle</span>
-            <span className='text-md sm:text-3xl font-[400] text-[#970404]'>Down</span>
+        <h1 className='flex flex-wrap items-baseline sm:items-end'>
+            <span className='text-xl sm:text-4xl text-transparent bg-clip-text font-[450] bg-gradient-to-r from-teal-400 from-40% to-teal-800'>Settle</span>
+            <span className='text-md sm:text-3xl font-[200] text-teal-800'>Down<span className='text-2xl font-[350] sm:text-5xl text-fuchsia-600'>.</span></span>
         </h1>
       </Link>
-        <form className='flex items-center bg-slate-100 rounded-lg px-3 py-2'>
-          <input type='text' placeholder='Search..' className='font-["Outfit"] bg-transparent focus:outline-none font-[300] w-24 sm:w-60 text-sm sm:text-md'/>
+        <form className='flex items-center bg-slate-100 rounded-lg px-3 py-2 md:-ml-10'>
+          <input type='text' placeholder='Search..' className='bg-transparent focus:outline-none font-[300] w-24 sm:w-60 text-sm sm:text-md'/>
           <button type='submit'><IoSearchOutline /></button>
         </form>
-        <ul className='flex space-x-6 font-["Outfit"] font-[300] text-md text-slate-600'>
+        <ul className='flex space-x-7 font-[300] text-md text-slate-600'>
             <Link to="/"><li className='hover:underline hover:decoration-1 hidden sm:inline'>Home</li></Link>
             <Link to="/about"><li className='hover:underline hover:decoration-1 hidden sm:inline'>About</li></Link>
             <Link to="/sign-in"><li className='hover:underline hover:decoration-1'>SignIn</li></Link>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { FaGoogle } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='max-w-sm sm:max-w-md mx-auto py-3 sm:py-5 px-8 rounded-2xl mt-4 sm:mt-7 shadow-md sm:shadow-xl shadow-gray-300  bg-white'>
+      <h1 className='text-center text-4xl font-light mb-7 mt-3 text-zinc-800'>Sign Up</h1>
+      <form className='flex flex-col gap-5'>
+        <input type='text' placeholder='Username' className='border p-3 rounded-lg focus:outline-none focus:border-teal-400 transition ease-in duration-200' id='username' />
+        <input type='email' placeholder='Email' className='border p-3 rounded-lg focus:outline-none focus:border-teal-400 transition ease-in duration-200' id='email' />
+        <input type='password' placeholder='Password' className='border p-3 rounded-lg focus:outline-none focus:border-teal-400 transition ease-in duration-200' id='password' />
+        <button type='submit' className='border border-teal-500 transition ease-in duration-150 hover:bg-teal-500 text-teal-700 hover:text-white rounded-3xl p-3'>Sign Up</button>
+        <button type='' className='flex justify-center items-center bg-rose-600 text-white p-3 rounded-3xl mb-4 hover:opacity-95'><FaGoogle /><span className='ml-2'>Continue with Google</span></button>
+      </form>
+      <p className='text-sm text-zinc-600 inline mr-2 ml-2'>Have an Account?</p>
+      <Link to={"/sign-in"}>
+        <span className='text-sm text-fuchsia-500 hover:underline hover:decoration-1 hover:underline-offset-4'>Sign In</span>
+      </Link>
+    </div>
   )
 }
 
