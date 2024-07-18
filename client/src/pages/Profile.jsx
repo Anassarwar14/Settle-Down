00dd20ffffe934a2b74ce19c5678cdf2bd8b3039
Mohammad Anas <Mohammad Anas@DESKTOP-VIT8S0V>
2024-07-18 11:52:39 +0500

    complete profile page UI and image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,148 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { app } from '../firebase';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import { MdOutlineModeEditOutline, MdOutlinePlaylistAdd, MdErrorOutline} from "react-icons/md";
+import { PiSignOut, PiSpinner } from "react-icons/pi";
+import { HiCheckBadge } from "react-icons/hi2";
+
+
+
+  // firebase storage rules code:
+  // allow read;
+  // allow write: if
+  // request.resource.size < 2 * 1024 * 1024 && 
+  // request.resource.contentType.matches('image/.*');
 
 function Profile() {
+  const loading = "";
+  const error = "";
+  const { currentUser } = useSelector((state) => state.user);
+  const [editMode, setEditMode] = useState(false);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [uploadError, setUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  const fileRef = useRef(null);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file])
+  
+
+  function handleFileUpload (file) {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name // added date to prevent double upload of the same file
+    const storageRef = ref(storage, fileName)
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then
+        ((downloadURL) => {
+            setFormData((prevState) => {
+                return {...prevState, avatar:downloadURL} 
+            })
+        })
+      }
+    );
+  }
+
+  function handleChange () {
+
+  }
+  
+  function handleSubmit () {
+
+  }
+
+  function toggleEdit () {
+    setEditMode(!editMode);
+  }
+  
+
   return (
-    <div>Profile</div>
+    <div className='max-w-md sm:max-w-6xl py-3 sm:py-5 px-8 mx-auto mt-4 sm:mt-7 mb-4 bg-gray-100 shadow-sm border rounded-md'>
+        <div className="flex items-center justify-between">
+          <h1 className='text-2xl font-light text-zinc-700'>My Profile</h1>
+          <button className='flex items-center text-sm gap-1 text-rose-600 hover:underline shadow-sm sm:shadow-md rounded-full px-4 py-1'>Sign Out<PiSignOut /></button>
+        </div>
+      <section className='flex items-center gap-4 max-w-md sm:max-w-6xl mx-auto mt-2 sm:mt-5 bg-gradient-to-tr from-purple-400 from-20% to-indigo-500 animated-background rounded-xl'>
+        <img src={ currentUser.avatar } alt='' className='max-w-[6rem] object-cover rounded-full'/>
+        <div className='flex-col'>
+          <h2 className='text-xl uppercase text-white' >{currentUser.username}</h2>
+        </div>
+      </section>
+
+      <div className='mt-4 rounded-lg border p-5'>
+        <div className="flex items-center justify-between">
+          <h2 className='ml-2 text-zinc-700'>Personal Info</h2>
+          <button className='flex items-center text-sm font-light gap-1 text-zinc-500 bg-gray-100 border shadow-md rounded-full px-4 py-1 hover:animate-pulse' onClick={toggleEdit}>Edit<MdOutlineModeEditOutline /></button>
+        </div>
+      </div>
+      <div className={`grid grid-cols-2 justify-items-center mt-5 mb-5 gap-3 sm:gap-1 ${editMode && 'hidden'}`}>
+        <h3 className='mt-4 text-md sm:text-lg text-zinc-400'>Username</h3>
+        <h3 className='mt-4 text-md sm:text-lg text-zinc-400'>Email Address</h3>
+        <h3 className='text-zinc-700 text-sm sm:text-md'>{currentUser.username}</h3>
+        <h3 className='text-zinc-700 text-sm sm:text-md'>{currentUser.email}</h3>
+      </div>
+
+      <form className={`flex flex-col gap-4 mt-5 px-4 ${!editMode && 'hidden'}`} onSubmit={ handleSubmit }>
+        <input 
+          onChange={(e) => setFile(e.target.files[0])}
+          type='file'
+          ref={fileRef}
+          hidden
+          accept='image/*'
+        />
+        <img src={ formData.avatar || currentUser.avatar } alt='display-img' onClick={() => fileRef.current.click()} className='max-w-[6rem] object-cover rounded-full cursor-pointer mx-auto'/>
+        <p className='*:flex *:items-center *:gap-2 *:justify-center *:font-light *:text-sm'>
+          { uploadError ? 
+            <span className='text-red-500'><MdErrorOutline />Error Uploading image! - Max size allowed: 2 MB</span> :
+            filePerc > 0  && filePerc < 100 ?
+            <span className='text-zinc-500'><PiSpinner className='animate-spin'/>Uploading... {filePerc}% completed</span> :
+            filePerc === 100 &&
+            <span className='text-green-500'><HiCheckBadge /> Image Uploaded Successfully!</span>
+          }
+        </p>
+        <input type='text' placeholder='Username' className='border p-3 rounded-lg hover:bg-gray-200 focus:outline-none focus:border-purple-400 transition ease-in duration-200' id='username' onChange={handleChange}/>
+        <input type='email' placeholder='Email' className='border p-3 rounded-lg hover:bg-gray-200 focus:outline-none focus:border-purple-400  transition ease-in duration-200' id='email' onChange={handleChange}/>
+        <input type='password' placeholder='Password' className='border p-3 rounded-lg hover:bg-gray-200 focus:outline-none focus:border-purple-400  transition ease-in duration-200' id='password' onChange={handleChange}/>
+        {error && <p className='text-red-600 text-sm ml-2'><MdErrorOutline />{error}</p>}
+        <button type='submit' disabled={loading} className='border border-purple-500 transition ease-in duration-150 hover:bg-purple-500 text-purple-700 hover:text-white rounded-3xl p-3 mb-2 min-w-60 mx-auto'>
+          {loading ? 
+          <div className='flex items-center justify-center'>
+            <svg aria-hidden="true" role="status" className="w-4 h-4 me-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2"/>
+            </svg> Loading...
+          </div> : "Update"}
+        </button>
+      </form>
+
+      <div className='mt-8 rounded-lg border p-5'>  
+        <h2 className='ml-2 text-zinc-700'>Listings</h2>
+      </div>
+      <button type='button' className='border border-purple-500 ml-4 transition ease-in duration-150 hover:border-white hover:bg-gradient-to-r hover:from-purple-400 hover:from-2% hover:to-indigo-500 text-purple-700 hover:text-white rounded-3xl px-4 py-2 mt-3'>
+        <div className='flex items-center gap-1'>
+          <MdOutlinePlaylistAdd  className='text-lg'/>
+          Create Listing
+        </div>
+      </button>
+      <hr className='mt-4 mb-2 '/>
+      <Link to={"/sign-in"}>
+        <span className='text-sm flex items-center justify-center text-red-600 hover:underline hover:decoration-1 hover:underline-offset-4'>Delete Account</span>
+      </Link>
+    </div>
   )
 }
 
