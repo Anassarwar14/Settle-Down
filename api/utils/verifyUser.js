222e07ed0e0b8c2071b8fff2b8c274374f5dbddc
Mohammad Anas <Mohammad Anas@DESKTOP-VIT8S0V>
2024-07-20 10:47:42 +0500

    create update user api route

---
+++ api/utils/verifyUser.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+import { errorHandler } from "./error.js";
+
+export const verifyToken = async (req, res, next) => {
+    const token = req.cookies.access_token;
+    if (!token) return next(errorHandler(401, 'Unauthorized'));
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, userID) => {
+        if (err) return errorHandler(403, 'Forbidden!');
+        req.user = userID;
+        next();
+    })
+}
